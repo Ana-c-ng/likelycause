8042cf2a95cce32a990c0f3f834adbd807483721
Ana-c-ng <59626414+Ana-c-ng@users.noreply.github.com>
2020-06-29 10:24:54 -0700

    Create setup.py

---
+++ likelycause/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='likelycause',
+    url='https://github.com/Ana-c-ng/likelycause',
+    author='Ana Garcia',
+    author_email='ana.n.garcia2@gmail.com',
+    # Needed to actually package something
+    packages=['likelycause'],
+    # Needed for dependencies
+    install_requires=['scipy','scipy.stats', 'sklearn.preprocessing', 'statsmodels.distributions.empirical_distribution'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='A quick way to get the likely cause',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
