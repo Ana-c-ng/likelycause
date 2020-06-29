0cb27e1b9bb7572bcb44bca5def37dac7c803325
Ana-c-ng <59626414+Ana-c-ng@users.noreply.github.com>
2020-06-29 11:48:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    # Needed to silence warnings (and to be a worthwhile package)
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Ana Garcia",
+    author_email="ana.n.garcia2@gmail.com",
     name='likelycause',
+    license="MIT",
+    description='Likely caue has a clever way to identify suspicious',
+    version='v0.0.1',
+    long_description=README,
     url='https://github.com/Ana-c-ng/likelycause',
-    author='Ana Garcia',
-    author_email='ana.n.garcia2@gmail.com',
-    # Needed to actually package something
-    packages=['likelycause'],
-    # Needed for dependencies
-    install_requires=['scipy','sklearn', 'statsmodels'],
-    # *strongly* suggested for sharing
-    version='0.1',
-    # The license can be anything you like
-    license='MIT',
-    description='A quick way to get the likely cause',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['scipy','sklearn','statsmodels'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
 )
