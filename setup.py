8ec8ed7a0d7973f76343ae3a67c0c3de44f94fba
Ana-c-ng <59626414+Ana-c-ng@users.noreply.github.com>
2020-06-29 11:54:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setuptools.setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2.7.10'
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Intended Audience :: Developers',
